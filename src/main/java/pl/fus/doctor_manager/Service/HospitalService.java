465907c27620f3b140a8a6829e7c717c2bd16dab
fus.maciek <fus.maciek@gmail.com>
2023-01-27 21:12:24 +0100

    Metoda put dla hospital działa :)

--- src/main/java/pl/fus/doctor_manager/Service/HospitalService.java
+++ src/main/java/pl/fus/doctor_manager/Service/HospitalService.java
@@ -3,11 +3,12 @@ package pl.fus.doctor_manager.Service;
 import org.springframework.stereotype.Service;
 import pl.fus.doctor_manager.DTO.HospitalDto;
 import pl.fus.doctor_manager.DtoMapper.HospitalMapper;
+import pl.fus.doctor_manager.Entity.Address;
 import pl.fus.doctor_manager.Entity.Hospital;
+import pl.fus.doctor_manager.Repository.AddressRepo;
 import pl.fus.doctor_manager.Repository.HospitalRepo;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 
@@ -15,10 +16,12 @@ import java.util.Optional;
 public class HospitalService {
     private final HospitalRepo hospitalRepo;
     private final HospitalMapper hospitalMapper;
+    private final AddressRepo addressRepo;
 
-    public HospitalService(HospitalRepo hospitalRepo, HospitalMapper hospitalMapper) {
+    public HospitalService(HospitalRepo hospitalRepo, HospitalMapper hospitalMapper, AddressRepo addressRepo) {
         this.hospitalRepo = hospitalRepo;
         this.hospitalMapper = hospitalMapper;
+        this.addressRepo = addressRepo;
     }
 
     public HospitalDto save(HospitalDto hospitalDto) {
@@ -39,6 +42,14 @@ public class HospitalService {
         if (!hospitalRepo.existsById(id)) {
             return Optional.empty();
         }
+        Hospital hospital = hospitalRepo.findById(id).orElseThrow();
+//        dto.getAddress().setId(hospital.getAddress().getId());
+//        dto.getAddress().setLocation(hospital.getAddress().getLocation());
+//        dto.getAddress().setPostcode(hospital.getAddress().getPostcode());
+//        dto.getAddress().setStreet(hospital.getAddress().getStreet());
+        Address address = hospital.getAddress();
+        dto.getAddress().setId(address.getId());
+        addressRepo.save(dto.getAddress());
         dto.setId(id);
         Hospital hospToUpdate = hospitalMapper.map(dto);
         Hospital savedHospital = hospitalRepo.save(hospToUpdate);
